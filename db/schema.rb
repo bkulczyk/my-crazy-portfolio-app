37af9f108af7bf3e2d9b96160ef6679c5c351dd3
bkulczyk <bkulczyk83@gmail.com>
2023-05-20 17:07:59 +0000

    remove content column from items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_20_142654) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_20_170639) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -72,7 +72,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_20_142654) do
   create_table "items", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.text "content"
     t.boolean "show_on_portfolio"
     t.boolean "show_on_blog"
     t.boolean "show_in_calendar"
